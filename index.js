2dae34e97c6b505250c6925339386097b2afd7e2
Volodymyr <137513853+borysuk@users.noreply.github.com>
2023-07-17 14:18:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ var fs = require('fs');
 var slugify = require('slugify');
 // cookies see https://github.com/expressjs/cookie-parser
 var cookieParser = require('cookie-parser');
+// var cookieSession = require('cookie-session');
+var bodyParser = require('body-parser');
