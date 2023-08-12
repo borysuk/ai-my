0f8fc7077ccd8961339d842a5caddc452d72eeca
Volodymyr <137513853+borysuk@users.noreply.github.com>
2023-08-12 15:43:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -54,3 +54,4 @@ var upload = multer({ storage: storage });
 //local modules (JS files)
 // var utils = require('./utils');
 var SentimentAnalysis = require('./lib/sentiment-analysis.js');
+var EntityAnalysis = require('./lib/entity-analysis.js');
