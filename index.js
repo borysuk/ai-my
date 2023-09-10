ac9c985d2a1403b9922fcf9adde98a121b37dde9
Volodymyr <137513853+borysuk@users.noreply.github.com>
2023-09-10 16:46:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -84,3 +84,5 @@ app.set('port', (process.env.PORT || 5000));
 var cookiesSecretKey = (process.env.COOKIES_SECRET_KEY || 'cookiesSecret');
 
 app.use(cookieParser(cookiesSecretKey));
+
+//for now use cookie session (in-memory)
