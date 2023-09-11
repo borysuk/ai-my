1bcb92fe8b97fe7adbc251d9753a38b80e7d9b60
Volodymyr <137513853+borysuk@users.noreply.github.com>
2023-09-11 12:38:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -86,3 +86,4 @@ var cookiesSecretKey = (process.env.COOKIES_SECRET_KEY || 'cookiesSecret');
 app.use(cookieParser(cookiesSecretKey));
 
 //for now use cookie session (in-memory)
+app.use(session({  secret: cookiesSecretKey }));
