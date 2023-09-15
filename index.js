c3d64a5b98210414d1deb5aed8a8ff291c40aff8
Volodymyr <137513853+borysuk@users.noreply.github.com>
2023-09-15 13:27:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -92,3 +92,6 @@ app.use(session({  secret: cookiesSecretKey }));
 // app.use(session({
 //     store: new RedisStore(options),
 //     secret: 'secretdata'
+// }));
+
+app.use(bodyParser.urlencoded({ limit:'2mb', extended: true })); // for parsing
