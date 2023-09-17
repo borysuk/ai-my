74fd7d07d9df57503fa4faf32e13e50c45cd4fcc
Volodymyr <137513853+borysuk@users.noreply.github.com>
2023-09-17 15:39:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -100,3 +100,8 @@ app.use(express.static(__dirname + '/public'));
 
 
 // views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+app.use(compression());
