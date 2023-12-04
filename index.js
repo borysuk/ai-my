eb97bbb6f0feba269fdc6e34dc97c46f3ae2130b
Volodymyr <137513853+borysuk@users.noreply.github.com>
2023-12-04 12:22:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -72,3 +72,7 @@ createEJSTemplateDataDictionary = function (req, res) {
 
 //storage
 // var session = require('express-session');
+// var RedisStore = require('connect-redis')(session);
+
+var app = express();
+// var privateKey  = fs.readFileSync('./key.pem', 'utf8');
